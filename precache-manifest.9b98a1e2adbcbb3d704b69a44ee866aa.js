99501b3510d9f03c083ff1220e10edaaa00a0b59
Robert Reinert <robert.reinert@gmail.com>
2020-10-19 16:08:36 -0700

    Updates

---
+++ precache-manifest.9b98a1e2adbcbb3d704b69a44ee866aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3675dd271d3097a415c8c3b5655420a2",
+    "url": "/wheres-waldo/index.html"
+  },
+  {
+    "revision": "896b1e9c1c7e26325079",
+    "url": "/wheres-waldo/static/css/main.3ada83cb.chunk.css"
+  },
+  {
+    "revision": "ffc0ed16f2ba854c41d9",
+    "url": "/wheres-waldo/static/js/2.66cd3046.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/wheres-waldo/static/js/2.66cd3046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896b1e9c1c7e26325079",
+    "url": "/wheres-waldo/static/js/main.306bb083.chunk.js"
+  },
+  {
+    "revision": "b14359b6740c56bcf460",
+    "url": "/wheres-waldo/static/js/runtime-main.21f80316.js"
+  }
+]);
